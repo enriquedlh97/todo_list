c5f3a88729460789036c37222a2140b0efa5a971
enriquedlh97 <enriquedlh97@hotmail.com>
2021-05-24 18:16:35 -0700

    Creates project

---
+++ todo_list/todo_list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo_list
+//
+//  Created by user189351 on 5/24/21.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
