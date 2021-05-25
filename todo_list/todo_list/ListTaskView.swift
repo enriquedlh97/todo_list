41e250a1f532625e7ca8b934fc971886f219c299
enriquedlh97 <enriquedlh97@hotmail.com>
2021-05-25 12:25:53 -0700

    Adds button to change task

--- todo_list/todo_list/ListTaskView.swift
+++ todo_list/todo_list/ListTaskView.swift
@@ -12,21 +12,36 @@ struct ListTaskView: View {
     @ObservedObject var tasks = TaskModel()
     
     var body: some View {
-        NavigationView {
-            List(tasks.tasks) { task in
-                RowTaskView(task: task)
+        ZStack {
+            NavigationView {
+                List(tasks.tasks) { task in
+                    RowTaskView(task: task)
+                }
+                VStack {
+                    Spacer()
+                    NavigationLink(destination: AddEditTaskView(task: Task.dummy, mode: .addTask), label: {
+                        Image(systemName: "plus.circle.fill")
+                            .font(.system(size: 60.0))
+                            .foregroundColor(Color("Emerald"))
+                            .shadow(color: Color.black.opacity(0.3),
+                                    radius: 1,
+                                    x: 1,
+                                    y: 1)
+                            .padding(20)
+                    })
+                }
             }
             .listStyle(PlainListStyle())
             .navigationBarTitle("Tasks", displayMode: .inline)
             .navigationBarColor(UIColor(named: "ElectronBlue"), UIColor(named: "SwanWhite"))
-                .toolbar {
-                    // Es para poner el título
-                    ToolbarItem(placement: .principal) {
-                        Text("Tasks")
-                            .modifier(Title())
-                            .foregroundColor(Color("SwanWhite"))
-                    }
+            .toolbar {
+                // Es para poner el título
+                ToolbarItem(placement: .principal) {
+                    Text("Tasks")
+                        .modifier(Title())
+                        .foregroundColor(Color("SwanWhite"))
                 }
+            }
         }
     }
 }
