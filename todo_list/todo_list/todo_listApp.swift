dfa4902b5be930117f5d9e2625def14f3596ff45
enriquedlh97 <enriquedlh97@hotmail.com>
2021-05-24 18:40:25 -0700

    Adds firbase dependency and sets it up

--- todo_list/todo_list/todo_listApp.swift
+++ todo_list/todo_list/todo_listApp.swift
@@ -2,13 +2,20 @@
 //  todo_listApp.swift
 //  todo_list
 //
-//  Created by user189351 on 5/24/21.
+//  Created by Enrique Diaz de Leon Hicks on 5/24/21.
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct todo_listApp: App {
+    
+    init() {
+        // Makes sure firebase is working on execution
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
