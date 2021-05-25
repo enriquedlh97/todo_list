119f11d8ba3e427b624557791630c18d0e40f925
enriquedlh97 <enriquedlh97@hotmail.com>
2021-05-25 11:33:35 -0700

    Adds navigationbar modifier

---
+++ todo_list/todo_list/NavigationBarModifier.swift
@@ -0,0 +1,51 @@
+//
+//  NavigationBarModifier.swift
+//  todo_list
+//
+//  Created by Enrique Diaz de Leon Hicks on 5/25/21.
+//
+
+import SwiftUI
+
+struct NavigationBarModifier: ViewModifier {
+        
+    var backgroundColor: UIColor?
+    
+    
+    init(backgroundColor: UIColor?, fontColor: UIColor?) {
+        self.backgroundColor = backgroundColor
+        let coloredAppearance = UINavigationBarAppearance()
+        coloredAppearance.configureWithTransparentBackground()
+        coloredAppearance.backgroundColor = .clear
+        coloredAppearance.titleTextAttributes = [.foregroundColor: fontColor!]
+        coloredAppearance.largeTitleTextAttributes = [.foregroundColor: fontColor!]
+        
+        UINavigationBar.appearance().standardAppearance = coloredAppearance
+        UINavigationBar.appearance().compactAppearance = coloredAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
+        UINavigationBar.appearance().tintColor = fontColor
+
+    }
+    
+    func body(content: Content) -> some View {
+        ZStack{
+            content
+            VStack {
+                GeometryReader { geometry in
+                    Color(self.backgroundColor ?? .clear)
+                        .frame(height: geometry.safeAreaInsets.top)
+                        .edgesIgnoringSafeArea(.top)
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
+extension View {
+ 
+    func navigationBarColor(_ backgroundColor: UIColor?, _ fontColor: UIColor?) -> some View {
+        self.modifier(NavigationBarModifier(backgroundColor: backgroundColor, fontColor: fontColor))
+    }
+
+}
