9a4589d0ceba12eb745079a5fc12dd63ac8444de
enriquedlh97 <enriquedlh97@hotmail.com>
2021-05-25 09:29:52 -0700

    Adds fonts and modifiers for title, subtitle and label

---
+++ todo_list/todo_list/Modifiers.swift
@@ -0,0 +1,32 @@
+//
+//  Modifiers.swift
+//  todo_list
+//
+//  Created by Enrique Diaz de Leon Hicks on 5/25/21.
+//
+
+import SwiftUI
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.custom("Baloo2-Bold", size: 24))
+            .foregroundColor(Color("ElectronBlue"))
+    }
+}
+
+struct Subtitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.custom("Baloo2-Bold", size: 20))
+            .foregroundColor(Color("ElectronBlue"))
+    }
+}
+
+struct Label: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.custom("Baloo2-Regular", size: 20))
+            .foregroundColor(Color("ElectronBlue"))
+    }
+}
